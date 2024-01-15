aaea7f3472ac0e8ed6b293a4e2ffca91404c229c
Maberry00 <maberry30@gmail.com>
2024-01-15 13:54:48 -0600

    operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace OperatorExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 17 + 4;
+
+            Console.WriteLine(sum);
+            {
+
+                int c = 17;
+                int d = 4;
+
+                Console.WriteLine(c - d);
+            }
+            {
+                int divided = 17 / 4;
+
+                Console.WriteLine(divided);
+            }
+            {
+                int a = 17;
+                int b = 4;
+
+                Console.WriteLine(a * b);
+            }
+            int remainder = 17 % 4;
+            Console.WriteLine(remainder);
+            {
+                Console.WriteLine("Enter the radius of the circle: ");
+
+                double userInput = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(AreaOfCircle(userInput));
+            }
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
     }
 }
+
